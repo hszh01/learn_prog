bdec91b85a96fc75264e8f661f62e4ab4ecb4740
satanicBuddha <harrisonzhang01@gmail.com>
2017-10-28 15:00:12 -0500

    Adding to github

--- lesson22.py
+++ lesson22.py
@@ -1,4 +1,4 @@
-# Problem 1: Write a program that prints 'Hello World' to the screen. 
+## Problem 1: Write a program that prints 'Hello World' to the screen. 
 #print ("Hello World")
 
 # Problem 2: Write a program that asks the user for their name and greets them with their name.
